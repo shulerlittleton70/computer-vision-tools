4db0e46f95a48634ba5155af866f12a59e76c264
shulerlittleton70 <shulerlittleton70@gmail.com>
2025-05-09 11:18:08 -0400

    First Pass

--- base-modules/handtrackingmodule.py
+++ base-modules/handtrackingmodule.py
@@ -3,34 +3,49 @@ import time
 import cv2
 from constants import TEXT_COLOR, CONNECTION_COLOR, LANDMARK_COLOR
 
-cap = cv2.VideoCapture(0)
 
-mphands = mp.solutions.hands
-hands = mphands.Hands()
-mpDraw = mp.solutions.drawing_utils
+class handDetector():
+    def __init__(self,mode=False,maxHands=2,detectionCon=0.8,trackingCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackingCon = trackingCon
 
-pTime = 0
-cTime = 0
-
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-               #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                print(id, cx, cy)
-                #if id==0:
-                #cv2.circle(img, (cx, cy), 25, TEXT_COLOR, cv2.FILLED)
-
-            mpDraw.draw_landmarks(img, handLms, mphands.HAND_CONNECTIONS,mp.solutions.drawing_utils.DrawingSpec(color=LANDMARK_COLOR, thickness=8),mp.solutions.drawing_utils.DrawingSpec(color=CONNECTION_COLOR, thickness=4))
+        self.mphands = mp.solutions.hands
+        self.hands = self.mphands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackingCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
 
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                if draw:
+                self.mpDraw.draw_landmarks(img, handLms, self.mphands.HAND_CONNECTIONS,
+                                      self.mp.solutions.drawing_utils.DrawingSpec(color=LANDMARK_COLOR, thickness=8),
+                                     self.mp.solutions.drawing_utils.DrawingSpec(color=CONNECTION_COLOR, thickness=4))
+
+        return img
+
+                #for id, lm in enumerate(handLms.landmark):
+                    # print(id,lm)
+                    #h, w, c = img.shape
+                    #cx, cy = int(lm.x * w), int(lm.y * h)
+                    #print(id, cx, cy)
+                    # if id==0:
+                    # cv2.circle(img, (cx, cy), 25, TEXT_COLOR, cv2.FILLED)
+
+
+def main()
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        detector.findHands(img)
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
@@ -41,4 +56,6 @@ while True:
     cv2.waitKey(1)
 
 
+if __name__ == "__main__":
+    main()
 
